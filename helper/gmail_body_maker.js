06f039719161cb6feb5a8e4f193f9868b55dd0d3
Saeed <saeed.uni68@gmail.com>
2019-09-24 18:49:07 +0330

    Create gmail_body_maker.js

---
+++ helper/gmail_body_maker.js
@@ -0,0 +1,124 @@
+// Credit: https://github.com/EmilTholin/gmail-api-create-message-body
+// For Demonstarate: https://www.qcode.co.uk/post/70
+
+function createJson(threadId) {
+  const thread = threadId ? `"threadId": "${threadId}"\r\n` : ''
+
+  return [
+    'Content-Type: application/json; charset="UTF-8"\r\n\r\n',
+    `{\r\n${thread}}`
+  ].join('')
+}
+
+function createHeaders(headers) {
+  if ( !headers || headers.length === 0 )
+    return ''
+
+  const result = []
+
+  for (const h in headers) {
+    if (headers.hasOwnProperty(h)) {
+      result.push(`${h}: ${headers[h]}\r\n`)
+    }
+  }
+
+  return result.join('')
+}
+
+function createPlain(text) {
+  return [
+    'Content-Type: text/plain; charset="UTF-8"\r\n',
+    'MIME-Version: 1.0\r\n',
+    'Content-Transfer-Encoding: 7bit\r\n\r\n',
+    text
+  ].join('')
+}
+
+function createHtml(html) {
+  return [
+    'Content-Type: text/html; charset="UTF-8"\r\n',
+    'MIME-Version: 1.0\r\n',
+    'Content-Transfer-Encoding: 7bit\r\n\r\n',
+    html
+  ].join('')
+}
+
+function createAlternative(text, html) {
+  return [
+    'Content-Type: multipart/alternative; boundary="foo"\r\n\r\n',
+
+    '--foo\r\n',
+    createPlain(text),
+    '\r\n\r\n',
+
+    '--foo\r\n',
+    createHtml(html),
+    '\r\n\r\n',
+
+    '--foo--',
+  ].join('')
+}
+
+function createBody(text, html) {
+  if (text && html)
+    return createAlternative(text, html)
+
+  if (text)
+    return createPlain(text)
+
+  if (html)
+    return createHtml(html)
+  
+  return ''
+}
+
+function createAttachments(attachments) {
+  if ( !attachments || attachments.length === 0 )
+    return ''
+
+  let result = []
+
+  for (let i = 0; i < attachments.length; i++) {
+    const att     = attachments[i]
+    const attName = att.filename ? `; filename="${att.filename}"` : ''
+
+    result = result.concat([
+      '--foo_bar\r\n',
+      `Content-Type: ${att.type}\r\n`,
+      'MIME-Version: 1.0\r\n',
+      'Content-Transfer-Encoding: base64\r\n',
+      `Content-Disposition: attachment${attName}\r\n\r\n`,
+      `${att.content}\r\n\r\n`
+    ])
+  }
+
+  return result.join('')
+}
+
+
+module.exports = function(params) {
+  const json        = createJson(params.threadId)
+  const headers     = createHeaders(params.headers)
+  const body        = createBody(params.text, params.html)
+  const attachments = createAttachments(params.attachments)
+
+  return [
+    '--foo_bar_baz\r\n',
+    `${json}\r\n\r\n`,
+
+    '--foo_bar_baz\r\n',
+    'Content-Type: message/rfc822\r\n\r\n',
+
+    'Content-Type: multipart/mixed; boundary="foo_bar"\r\n',
+    `${headers}\r\n`,
+
+    '--foo_bar\r\n',
+    `${body}\r\n\r\n`,
+
+    attachments,
+
+    '--foo_bar--\r\n\r\n',
+
+    '--foo_bar_baz--',
+  ].join('')
+}
