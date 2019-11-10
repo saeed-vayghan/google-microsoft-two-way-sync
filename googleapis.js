8d3845415b0f612ca2eef24a8aff8f551a6676da
Saeed <saeed.uni68@gmail.com>
2019-11-10 16:29:57 +0330

    Update googleapis.js

--- googleapis.js
+++ googleapis.js
@@ -75,26 +75,15 @@ Google.prototype.getAndSetTokens = async function(code) {
 
 
 // Profile
-Google.prototype.getGmailProfile = async function() {
+Google.prototype.getGmailProfile = async function () {
   const response = await this.gmail.users.getProfile({ auth: this.oAuth2Client, userId: 'me' })
 
   return response.data
 }
 
 
-// Lables
-Google.prototype.listLabels = async function() {
-  const response = await this.gmail.users.labels.list({
-    auth: this.oAuth2Client,
-    userId: this.gmailAddress
-  })
-
-  return response.data
-}
-
-
 // History
-Google.prototype.discreteHistory = async function* (currentHistoryId = null) {
+Google.prototype.discreteHistory = async function* (limit, currentHistoryId = null) {
   let nextPageToken = null
   let response
 
@@ -103,7 +92,7 @@ Google.prototype.discreteHistory = async function* (currentHistoryId = null) {
 
       response = await this.gmail.users.history.list({
         userId: this.gmailAddress,
-        maxResults: 250, // max: 500
+        maxResults: limit, // max: 500
         historyTypes: ['messageAdded', 'messageDeleted'],
         startHistoryId: currentHistoryId,
         pageToken: nextPageToken
@@ -120,7 +109,7 @@ Google.prototype.discreteHistory = async function* (currentHistoryId = null) {
   } while (nextPageToken)
 }
 
-Google.prototype.history = async function(currentHistoryId = null) {
+Google.prototype.history = async function (currentHistoryId = null) {
   let history        = []
   let nextPageToken  = null
   let response
@@ -174,7 +163,7 @@ Google.prototype.discreteSyncThreads = async function* () {
   } while (nextPageToken)
 }
 
-Google.prototype.syncThreads = async function() {
+Google.prototype.syncThreads = async function () {
   let threads       = []
   let nextPageToken = null
   let response
@@ -200,7 +189,7 @@ Google.prototype.syncThreads = async function() {
   }
 }
 
-Google.prototype.getThread = async function(threadId) {
+Google.prototype.getThread = async function (threadId) {
   const response = await this.gmail.users.threads.get({
     auth: this.oAuth2Client,
     userId: this.gmailAddress,
@@ -213,7 +202,7 @@ Google.prototype.getThread = async function(threadId) {
 
 
 // Messages
-Google.prototype.discreteSyncMessages = async function* () {
+Google.prototype.discreteSyncMessages = async function* (limit) {
   let nextPageToken = null
   let response
 
@@ -222,7 +211,7 @@ Google.prototype.discreteSyncMessages = async function* () {
     response = await this.gmail.users.messages.list({
       userId: this.gmailAddress,
       includeSpamTrash: false,
-      maxResults: 250, // max: 500
+      maxResults: limit, // max: 500
       pageToken: nextPageToken
     })
 
@@ -233,7 +222,7 @@ Google.prototype.discreteSyncMessages = async function* () {
   } while (nextPageToken)
 }
 
-Google.prototype.syncMessages = async function() {
+Google.prototype.syncMessages = async function () {
   let messages      = []
   let nextPageToken = null
   let response
@@ -243,7 +232,7 @@ Google.prototype.syncMessages = async function() {
     response = await this.gmail.users.messages.list({
       userId: this.gmailAddress,
       includeSpamTrash: false,
-      maxResults: 250, // max: 500
+      maxResults: 50, // max: 500
       pageToken: nextPageToken
     })
 
@@ -263,7 +252,7 @@ Google.prototype.syncMessages = async function() {
   }
 }
 
-Google.prototype.batchGetMessages = async function(messages, fields) {
+Google.prototype.batchGetMessages = async function (messages, fields) {
   const authHeader = await this.oAuth2Client.getRequestHeaders()
   const multipart  = messages.map(message => ({
     'Content-Type': 'application/http',
@@ -283,7 +272,7 @@ Google.prototype.batchGetMessages = async function(messages, fields) {
   return extractor.extract(responseString)
 }
 
-Google.prototype.getMessage = async function(messageId) {
+Google.prototype.getMessage = async function (messageId) {
   const response = await this.gmail.users.messages.get({
     auth: this.oAuth2Client,
     userId: this.gmailAddress,
@@ -305,7 +294,7 @@ Google.prototype.getAttachment = async function (messageId, attachmentId) {
   return response.data
 }
 
-Google.prototype.sendMessage = async function(email) {
+Google.prototype.sendMessage = async function (email) {
   const message = [
     'Content-Type: text/plain; charset="UTF-8"',
     'MIME-Version: 1.0',
@@ -330,7 +319,7 @@ Google.prototype.sendMessage = async function(email) {
   return response
 }
 
-Google.prototype.sendMessageWithAttachment = async function(email) {
+Google.prototype.sendMessageWithAttachment = async function (email) {
   // File Size Limit: 5 MB
 
   const message = new MailComposer({
@@ -358,7 +347,7 @@ Google.prototype.sendMessageWithAttachment = async function(email) {
   return response
 }
 
-Google.prototype.sendMultipartMessage = async function(body) {
+Google.prototype.sendMultipartMessage = async function (body) {
   // File Size Limit: Up To 35 MB
   const authHeader = await this.oAuth2Client.getRequestHeaders()
   
@@ -377,9 +366,49 @@ Google.prototype.sendMultipartMessage = async function(body) {
   return JSON.parse(responseString)
 }
 
+Google.prototype.updateMessageLables = async function (messageId, labelsToAdd, labelsToRemove) {
+  const response = await this.gmail.users.messages.modify({
+    auth: this.oAuth2Client,
+    userId: this.gmailAddress,
+    id: messageId,
+    requestBody: {
+      addLabelIds: labelsToAdd,
+      removeLabelIds: labelsToRemove
+    }
+  })
 
-// People
-Google.prototype.getProfile = async function() {
+  return response.data
+}
+
+Google.prototype.markAsRead = async function(messageId) {
+  const labelsToAdd    = []
+  const labelsToRemove = ['UNREAD']
+
+  return this.updateMessageLables(messageId, labelsToAdd, labelsToRemove)
+}
+
+Google.prototype.markAsUnRead = async function(messageId) {
+  const labelsToAdd    = ['UNREAD']
+  const labelsToRemove = []
+
+  return this.updateMessageLables(messageId, labelsToAdd, labelsToRemove)
+}
+
+
+
+// Lables
+Google.prototype.listLabels = async function () {
+  const response = await this.gmail.users.labels.list({
+    auth: this.oAuth2Client,
+    userId: this.gmailAddress
+  })
+
+  return response.data
+}
+
+
+// People, Contacts (pageSize: between 1 and 2000, inclusive. Defaults to 100)
+Google.prototype.getProfile = async function () {
   const response = await this.people.people.get({
     resourceName: 'people/me',
     personFields: 'emailAddresses,names,photos',
@@ -388,7 +417,9 @@ Google.prototype.getProfile = async function() {
   return response.data
 }
 
-Google.prototype.listConnections = async function(currentSyncToken = null) {
+Google.prototype.listConnections = async function (currentSyncToken = null) {
+  const currentGmailAddress = this.gmailAddress
+
   const expTokenMsg  = 'Sync token is expired. Clear local cache and retry call without the sync token.'
   const personFields = 'metadata,addresses,birthdays,coverPhotos,emailAddresses,names,nicknames,organizations,phoneNumbers,photos,urls,memberships'
 
@@ -412,9 +443,12 @@ Google.prototype.listConnections = async function(currentSyncToken = null) {
 
     } catch (ex) {
 
-      // if ( ex.message !== expTokenMsg ) {
-      //   throw ex
-      // }
+      if ( ex.message !== expTokenMsg ) {
+        throw Error.Conflict({ details: {
+          attributes: { email: currentGmailAddress },
+          info: { method: 'google.listConnections', currentSyncToken: currentSyncToken }
+        }})
+      }
 
       response = await this.people.people.connections.list({
         personFields: personFields,
@@ -442,7 +476,7 @@ Google.prototype.listConnections = async function(currentSyncToken = null) {
   }
 }
 
-Google.prototype.listContactGroups = async function(currentSyncToken = null) {
+Google.prototype.listContactGroups = async function (currentSyncToken = null) {
   let contactGroups = []
   let syncToken     = currentSyncToken
   let pageToken
@@ -479,7 +513,7 @@ Google.prototype.listContactGroups = async function(currentSyncToken = null) {
 }
 
 
-// Conatacts API V.3
+// conatacts api v3
 Google.prototype.getContactGroups = async function () {
   const authHeader = await this.oAuth2Client.getRequestHeaders()
   
@@ -559,11 +593,170 @@ Google.prototype.getContactPhoto = async function (url) {
     }
   })
 
-  // Save the image
-  // const fs = require('fs').promises
-  // await fs.writeFile(`/pat/to/any-where//${new Date().getTime()}.jpg`, imagedata, 'binary')
-
   return imagedata
+}
+
+
+// Calendar
+Google.prototype.listCalendars = async function () {
+  const option = {
+    maxResults: 100,
+    pageToken: null,
+    showHidden: true,
+    showDeleted: false,
+    syncToken: null
+  }
+
+  const calendarList = await this.calendar.calendarList.list(option)
+  
+  return calendarList.data
+}
+
+Google.prototype.createCalendar = async function (resource) {
+  const result = await this.calendar.calendars.insert({ resource: resource })
+
+  return result.data
+}
+
+Google.prototype.getCalendarList = async function (calendarId) {
+  const result = await this.calendar.calendarList.get({ calendarId: calendarId })
+
+  return result.data
+}
+
+Google.prototype.getCalendar = async function (calendarId) {
+  const result = await this.calendar.calendars.get({ calendarId: calendarId })
+
+  return result.data
+}
+
+Google.prototype.updateCalendar = async function (calendarId, resource) {
+  const result = await this.calendar.calendars.update({ calendarId: calendarId, resource: resource })
+
+  return result.data
+}
+
+Google.prototype.deleteCalendar = async function (calendarId) {
+  return await this.calendar.calendars.delete({ calendarId: calendarId })
+}
+
+Google.prototype.watchCalendar = async function (options) {
+  const result = await this.calendar.events.watch(options)
+
+  return result.data
+}
+
+Google.prototype.stopWatchCalendar = async function (options) {
+  return await this.calendar.channels.stop(options)
+}
+
+
+// Calendar Events
+Google.prototype.listEvents = async function (calendarId) {
+  const response = await this.calendar.events.list({
+    calendarId: calendarId,
+    singleEvents: true,
+    maxResults: 10,
+    showHiddenInvitations: true,
+    showDeleted: true
+  })
+
+  return response.data.items
+}
+
+Google.prototype.syncEvents = async function (calendarId, currentSyncToken = null) {
+  let items         = []
+  let nextSyncToken = currentSyncToken
+  let nextPageToken = null
+  let response
+
+  do {
+    response = await this.calendar.events.list({
+      calendarId: calendarId,
+      singleEvents: true,
+      maxResults: 50,
+      showHiddenInvitations: true,
+      showDeleted: true,
+      pageToken: nextPageToken,
+      syncToken: nextSyncToken
+    })
+
+    nextPageToken = response.data.nextPageToken || null
+    nextSyncToken = response.data.nextSyncToken || null
+
+    if (response.data.items)
+      items = items.concat(response.data.items)
+
+  } while ( nextPageToken !== null )
+
+  return {
+    items,
+    nextSyncToken
+  }
+}
+
+Google.prototype.createEvent = async function (calendarId, resource) {
+  const result = await this.calendar.events.insert({
+    calendarId: calendarId,
+    resource: resource
+  })
+
+  return result.data
+}
+
+Google.prototype.getEvent = async function (calendarId, eventId) {
+  const result = await this.calendar.events.get({ calendarId: calendarId, eventId: eventId })
+
+  return result.data
+}
+
+Google.prototype.getEventInstances = async function (calendarId, eventId, options) {
+  let items         = []
+  let nextSyncToken = options.currentSyncToken
+  let nextPageToken = null
+  let response
+
+  do {
+
+    response = await this.calendar.events.instances({
+      calendarId: calendarId,
+      eventId: eventId,
+      maxResults: 100,
+      showDeleted: true,
+
+      timeMax: options.timeMax,
+      timeMin: options.timeMin,
+
+      pageToken: nextPageToken,
+      syncToken: nextSyncToken
+    })
+
+    nextPageToken = response.data.nextPageToken || null
+    nextSyncToken = response.data.nextSyncToken || null
+
+    if (response.data.items)
+      items = items.concat(response.data.items)
+
+  } while ( nextPageToken !== null )
+
+  return {
+    items,
+    nextSyncToken
+  }
+}
+
+Google.prototype.updateEvent = async function (calendarId, eventId, resource) {
+  const result = await this.calendar.events.update({
+    calendarId: calendarId,
+    eventId: eventId,
+    resource: resource
+  })
+
+  return result.data
+}
+
+Google.prototype.deleteEvent = async function (calendarId, eventId) {
+  return await this.calendar.events.delete({ calendarId: calendarId, eventId: eventId })
 }
 
 
