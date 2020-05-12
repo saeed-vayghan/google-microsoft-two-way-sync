e3752d93a4322431e3d2afddedd248b24583d9d9
Saeed Vayghani <saeed.uni68@gmail.com>
2020-05-12 16:34:49 +0430

    Update extract_json.js

--- helper/extract_json.js
+++ helper/extract_json.js
@@ -1,16 +1,80 @@
 /* 
-  Credit: https://stackoverflow.com/questions/10574520/extract-json-from-text
-  I made some changes into main script
+  Credit: 
+    https://github.com/esoodev
+    https://stackoverflow.com/questions/10574520/extract-json-from-text
 */
 
 module.exports = {
 
-  extract: function(str) {
+  _getOpenCloseBraceIndex: function (str, indexStart, openBrace, closeBrace) {
+    const openIndex = str.substr(indexStart).indexOf(openBrace) + indexStart
+
+    if (!openIndex)
+      return null
+
+    let openCount  = 1
+    let closeCount = 0
+    let currIndex  = openIndex + 1
+
+    while (str[currIndex]) {
+
+      if (str[currIndex] === openBrace) {
+        openCount ++
+        currIndex ++
+      
+        continue
+
+      } else if (str[currIndex] === closeBrace) {
+
+        closeCount ++
+
+        if (openCount === closeCount) {
+          return [openIndex, currIndex]
+        }
+      }
+
+      currIndex ++
+    }
+
+    return null
+  },
+
+  extract: function (str) {
+    let currIndex = 0
+    const objs    = []
+
+    while (str[currIndex]) {
+      const openClose = this._getOpenCloseBraceIndex(str, currIndex, '{', '}')
+
+      if (openClose) {
+        const objStr = str.substr(openClose[0], openClose[1] - openClose[0] + 1)
+
+        try {
+          const parsed = JSON.parse(objStr)
+          
+          if (parsed)
+            objs.push(parsed)
+            
+          currIndex = openClose[1]
+
+        } catch (err) {
+          currIndex ++
+        }
+
+      } else {
+        currIndex ++
+      }
+    }
+
+    return objs
+  },
+
+  extractJSON: function(str) {
     const result = []
 
     let firstOpen  = 0
     let firstClose = 0
-    let candidate  = 0
+    let candidate  = ''
   
     firstOpen = str.indexOf('{', firstOpen + 1)
   
@@ -18,14 +82,14 @@ module.exports = {
       firstClose = str.lastIndexOf('}')
   
       if ( firstClose <= firstOpen ) {
-        return null
+        return []
       }
   
       do {
         candidate = str.substring(firstOpen, firstClose + 1)
   
         try {
-          result.push(JSON.parse(candidate));
+          result.push(JSON.parse(candidate))
           firstOpen = firstClose
         } catch (e) {
           firstClose = str.substr(0, firstClose).lastIndexOf('}')
@@ -35,7 +99,7 @@ module.exports = {
   
       firstOpen = str.indexOf('{', firstOpen + 1)
   
-    } while ( firstOpen != -1 )
+    } while ( firstOpen !== -1 )
   
     return result
   }
